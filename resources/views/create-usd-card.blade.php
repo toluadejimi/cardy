bed2cf9d851acdb64724830c36ac15533e2a2c4c
jimmy <toluadejimi@gmail.com>
2022-10-05 13:28:54 +0100

    update

--- resources/views/create-usd-card.blade.php
+++ resources/views/create-usd-card.blade.php
@@ -249,6 +249,17 @@
 
             @endif
 
+
+
+
+
+
+
+
+
+
+        </div>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
 <script>
@@ -262,13 +273,6 @@ $('input').keyup(function(){ // run anytime the value changes
 // add them and output it
 });
 </script>
-
-
-
-
-
-
-
-
-        </div>
         @endsection
+
+
