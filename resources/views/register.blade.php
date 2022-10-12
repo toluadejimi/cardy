ecc03e8c06a5f243611392551f38811bfc2dadb8
jimmy <toluadejimi@gmail.com>
2022-10-12 17:24:53 +0100

    updated

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -120,7 +120,7 @@
                 <label class="form-label" for="basic-icon-default-fullname">Enter Your Phone NUmber</label>
                 <div class="input-group input-group-merge">
                   <span id="basic-icon-default-fullname2" class="input-group-text"><i class="bx bx-phone"></i></span>
-                  <input type="number" class="form-control" value="234" name="phone" id="basic-icon-default-fullname"  required placeholder="123 456 789" aria-label="" aria-describedby="basic-icon-default-fullname2" />
+                  <input type="number"  required class="form-control" value="234" name="phone" id="basic-icon-default-fullname"  required placeholder="123 456 789" aria-label="" aria-describedby="basic-icon-default-fullname2" />
                 </div>
               </div>
 
@@ -129,7 +129,7 @@
                 <label class="form-label" for="basic-icon-default-fullname">Surname</label>
                 <div class="input-group input-group-merge">
                   <span id="basic-icon-default-fullname2" class="input-group-text"><i class="bx bx-user"></i></span>
-                  <input type="text" class="form-control"requried  autofocus  name="l_name" id="l_name" placeholder="Enter your  surname" aria-label="" aria-describedby="basic-icon-default-fullname2" />
+                  <input type="text"  required class="form-control"requried  autofocus  name="l_name" id="l_name" placeholder="Enter your  surname" aria-label="" aria-describedby="basic-icon-default-fullname2" />
                 </div>
               </div>
 
@@ -138,14 +138,14 @@
                 <label class="form-label" for="basic-icon-default-fullname">First Name</label>
                 <div class="input-group input-group-merge">
                   <span id="basic-icon-default-fullname2" class="input-group-text"><i class="bx bx-user"></i></span>
-                  <input type="text" class="form-control"requried  autofocus  name="f_name" id="f_name" placeholder="Enter your  First name" aria-label="" aria-describedby="basic-icon-default-fullname2" />
+                  <input type="text" required class="form-control"requried  autofocus  name="f_name" id="f_name" placeholder="Enter your  First name" aria-label="" aria-describedby="basic-icon-default-fullname2" />
                 </div>
               </div>
 
               <div class="mb-3">
                 <label class="form-label" for="basic-icon-default-fullname">Gender</label>
                 <div class="input-group input-group-merge">
-                  <span id="basic-icon-default-fullname2" class="input-group-text"><i class="bx bx-user"></i></span>
+                  <span id="basic-icon-default-fullname2"  required class="input-group-text"><i class="bx bx-user"></i></span>
                   <select name="gender" id="" class="form-control">
                   <option value="">Select your gender</option>
                   <option value="male">Male</option>
