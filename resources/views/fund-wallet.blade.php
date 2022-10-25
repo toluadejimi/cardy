3dfc12d053a29f1925b9356e2d3f7d092f4115b8
jimmy <toluadejimi@gmail.com>
2022-10-25 07:16:00 +0100

    commented

--- resources/views/fund-wallet.blade.php
+++ resources/views/fund-wallet.blade.php
@@ -104,7 +104,7 @@
                         </div>
 
                         <div class="mb-3">
-                          <button type="button" id="start-payment-button" class="btn btn-primary" onclick="makePayment()">Continue</button>
+                          <button type="button" id="start-payment-button" class="btn btn-primary" onclick="#">Continue</button>
                         </div>
                       </form>
                     </div>
