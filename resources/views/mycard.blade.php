f11147a31674f29afeaa8e3e6cff041b5c9b9a67
jimmy <toluadejimi@gmail.com>
2022-10-27 21:25:39 +0100

    uu

--- resources/views/mycard.blade.php
+++ resources/views/mycard.blade.php
@@ -5,7 +5,6 @@
     <!-- Content wrapper -->
     <div class="content-wrapper">
         <!-- Content -->
-
         <div class="container-xxl flex-grow-1 container-p-y">
             <div class="row">
                 <div class="col-lg-12 mb-4 order-0">
