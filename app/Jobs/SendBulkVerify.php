b4a0f1b797943f9c7a0e8ffd24c3205d96b7dfc2
jimmy <toluadejimi@gmail.com>
2022-11-16 01:27:47 +0100

    update

--- app/Jobs/SendBulkVerify.php
+++ app/Jobs/SendBulkVerify.php
@@ -3,11 +3,11 @@
 namespace App\Jobs;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use App\Models\User;
 
 class SendBulkVerify implements ShouldQueue
 {
